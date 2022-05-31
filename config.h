91bda329a71e4a94ccdfc77d332424c400388384
ivandimitrov8080 <ivan@ivandimitrov.co.uk>
2022-05-31 14:43:47 +0000

    transparency change

--- config.h
+++ config.h
@@ -92,7 +92,7 @@ char *termname = "st-256color";
  *	stty tabs
  */
 unsigned int tabspaces = 8;
-float alpha = 0.85;
+float alpha = 0.75;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
